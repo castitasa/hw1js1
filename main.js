46456f806a5da8329b71a40a588775fc1a051a67
castitasa <aibike0806@gmail.com>
2023-08-29 21:08:10 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let num ='11';
+
+if(num % 2 ==0){
+    console.log('Четное число');
+} else if(num % 3 ==0  ){
+    console.log('Нечетное число');
+} else if(num % 2 ==1){
+    console.log('Нечетное число');
+} else{
+    console.log('Введите только цифры');
+}
